48b8234b5d8b8e88e86f0bde9e7c74401242a577
Erick <eeramirez@tuvansa.com.mx>
2023-11-26 14:01:17 -0600

    add server conf  https in ubuntu

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express, { json } from 'express'
+import https from 'node:https'
+import http from 'node:http'
+import fs from 'node:fs'
 import cors from 'cors'
 import 'dotenv/config'
 import { userRouter } from './routes/user'
@@ -13,9 +16,33 @@ const app = express()
 
 conectDB()
 
+const existPathCert = fs.existsSync('/etc/letsencrypt/live/tuvansacloud.dyndns.org')
+let credentials = {
+  key: '',
+  cert: '',
+  ca: ''
+}
+
+
+if (existPathCert) {
+  let privateKey = fs.readFileSync('/etc/letsencrypt/live/tuvansacloud.dyndns.org/privkey.pem', 'utf8')
+  let certificate = fs.readFileSync('/etc/letsencrypt/live/tuvansacloud.dyndns.org/cert.pem', 'utf8');
+  let ca = fs.readFileSync('/etc/letsencrypt/live/tuvansacloud.dyndns.org/chain.pem', 'utf8');
+
+  credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca
+  }
+
+}
+
 
 app.use(cors())
 app.use(json())
+
+app.use(express.static(join(__dirname), { dotfiles: 'allow' }));
+
 app.use('/public', express.static(join(__dirname, '../uploads')))
 
 app.use('/public', express.static(join(__dirname, './uploads')))
@@ -27,6 +54,26 @@ app.use('/api/files', FileRouter)
 app.use('/api/proscai', proscaiRouter)
 app.use(handleErrors)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listen in port ${process.env.PORT}`)
-})
+if (existPathCert) {
+
+  const httpServer = http.createServer(app)
+  const httpsServer = https.createServer(credentials, app)
+
+  httpServer.listen(80, () => {
+    console.log(`Server listen in port 80`)
+  })
+
+
+  httpsServer.listen(443, () => {
+    console.log('Server https running on port 443')
+  })
+
+} else {
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server listen in port ${process.env.PORT}`)
+  })
+
+}
+
+
